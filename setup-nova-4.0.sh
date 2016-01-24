f220dc315daacd2cb433b12d3394b8f20e7b889c
Andiry Xu <jix024@cs.ucsd.edu>
2016-01-23 22:38:45 -0800

    Fix 4.0 setup-nova script

--- setup-nova-4.0.sh
+++ setup-nova-4.0.sh
@@ -6,5 +6,6 @@ insmod nova.ko measure_timing=0
 
 sleep 1
 
-mount -t NOVA -o physaddr=0x300000000,init=4G NOVA /mnt/ramdisk
+#mount -t NOVA -o physaddr=0x300000000,init=4G NOVA /mnt/ramdisk
+mount -t NOVA -o physaddr=0x10000000000,init=64G none /mnt/ramdisk
 
